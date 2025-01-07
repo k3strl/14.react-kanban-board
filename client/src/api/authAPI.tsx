3edbf8cb1a9bfca9415e3df6edc7918439b2befd
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-01-06 18:12:52 -0600

    added authAPI.tsx code

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,31 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch("/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    // If response not ok, throw error
+    if (!response.ok) {
+
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+    // Parse response as json
+    const loginData = await response.json();
+
+    return loginData;
+
+    // Error handling
+  } catch (err) {
+    console.log(`Error during login:`, err);
+    return Promise.reject(`Check login info`);
+  }
 }
-
-
 
 export { login };
